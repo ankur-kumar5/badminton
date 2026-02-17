802570ac56f44fdb94b81118ad33f61a80d38d22
ankur-kumar5 <134390248+ankur-kumar5@users.noreply.github.com>
2026-02-17 16:14:51 +0530

    Add association between Player and Match Models

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,14 @@
 class Match < ApplicationRecord
+  belongs_to :winner, class_name: "Player"
+  belongs_to :loser, class_name: "Player"
+
+  validate :players_must_be_different
+
+  private
+
+  def players_must_be_different
+    return unless winner_id == loser_id
+
+    errors.add(:base, I18n.t("matches.different_player"))
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,13 @@
 class Player < ApplicationRecord
+  has_many :won_matches,
+           class_name: "Match",
+           foreign_key: :winner_id,
+           dependent: :restrict_with_error
+
+  has_many :lost_matches,
+           class_name: "Match",
+           foreign_key: :loser_id,
+           dependent: :restrict_with_error
+
+  validates :name, presence: true, uniqueness: true
 end
