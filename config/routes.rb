94c66d629b93581d08c7cfef583c6021e54847d4
ankur-kumar5 <134390248+ankur-kumar5@users.noreply.github.com>
2026-02-17 16:23:31 +0530

    Define RESTful resource routes and configure root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'matches/index'
-  get 'matches/create'
-  get 'players/index'
-  get 'players/create'
-  get 'players/destroy'
+  root "players#index"
+  resources :players, only: %i[index create destroy]
+  resources :matches, only: %i[index create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
