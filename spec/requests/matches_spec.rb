1d8741f841f7c2e5495ce997df0fb82c05ed85ee
ankur-kumar5 <134390248+ankur-kumar5@users.noreply.github.com>
2026-02-17 16:20:59 +0530

    Add controllers and corresponding views

---
+++ spec/requests/matches_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Matches", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/matches/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/matches/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
