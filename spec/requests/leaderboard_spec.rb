25f1a6bb3da072bf4a6c8f3749bdb819f17e8f65
ankur-kumar5 <134390248+ankur-kumar5@users.noreply.github.com>
2026-02-17 17:41:20 +0530

    Implemented Default leaderboard index RSpec

---
+++ spec/requests/leaderboard_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Leaderboards", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/leaderboard/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
